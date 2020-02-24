43b1359da544623b3e664746372cfc21aad88368
Bui Minh Quang <bmq928@gmail.com>
2020-02-24 16:01:50 +0700

    initial

---
+++ background.js
@@ -0,0 +1,3 @@
+// for process background
+// nothing need for background proces for this extension
+console.log('nahs')
---
+++ toggle-panel.js
@@ -0,0 +1,22 @@
+document.addEventListener('keydown', event => {
+  if (!isKibanaPage) return
+  whenCtrlAndSemiPressed(event, () => togglePanel())
+})
+
+function whenCtrlAndSemiPressed(event, cb) {
+  if (event.ctrlKey && event.keyCode === 186) {
+    cb()
+  }
+}
+
+function isKibanaPage() {
+  return document.body.getAttribute('id') === 'kibana-app'
+}
+
+function togglePanel() {
+  const [queryPanel, resultPanel] = document.querySelectorAll('textarea')
+  const focused = document.activeElement
+
+  if (focused !== queryPanel) return queryPanel.focus()
+  resultPanel.focus()
+}
